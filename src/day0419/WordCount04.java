77db60221243a67991f4cee618a842b96ed3da41
kyunghee28 <pori4339@naver.com>
2019-04-19 20:36:17 +0900

    java

--- src/day0419/WordCount04.java
+++ src/day0419/WordCount04.java
@@ -1,17 +1,18 @@
 package day0419;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.StringTokenizer; 
 public class WordCount04 {
 
 	public static void main(String[] args) {
 		
-		String fname1 = args[0];
+		String fname = args[0];
 	
 		try {
 			
-			BufferedReader br = new BufferedReader(new FileReader(fname1));
-			
+			BufferedReader br = new BufferedReader(new FileReader(fname));
 			String lineDate;		
 			StringBuffer sb = new StringBuffer();
 			
@@ -23,9 +24,33 @@ public class WordCount04 {
 			String d = sb.toString();
 			
 			StringTokenizer st = new StringTokenizer(d);
-			System.out.println(st.countTokens());
-			br.close();
+			HashMap<String, Integer> map = new HashMap<String, Integer>();
 			
+			while(st.hasMoreElements())
+			{
+				String word = st.nextToken();
+				Integer cnt = map.get(word);
+				
+				if(cnt == null) // 첫 단어 일때 
+				{
+					map.put(word,1);
+				}
+				else
+				{
+					map.put(word, cnt+1);
+				}
+			}
+			
+			Iterator keys = map.keySet().iterator();
+			while(keys.hasNext())
+			{
+				String k = (String)keys.next();
+				Integer values = map.get(k);
+				System.out.println(k+"\t"+values);
+			}
+			
+			br.close();
+			System.out.println("프로그램 종료");
 		}catch(Exception e) {}
 	}
 }
